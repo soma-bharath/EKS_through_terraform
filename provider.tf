8ba04448a526b668d0c4d06e4c15b357d1c91a28
soma-bharath <72080617+soma-bharath@users.noreply.github.com>
2023-10-30 19:23:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,3 +10,16 @@ terraform {
 provider "aws" {
 
 }
+
+
+resource "null_resource" "argo_install" {
+  provisioner "local-exec" {
+    command = <<EOT
+      sudo chmod 777 mine.sh
+      sudo bash ${path.module}/EKS_through_terraform/mine.sh
+    EOT
+
+    #working_dir = "/tmp"  # You can change the working directory as needed
+  }
+depends_on=[aws_eks_cluster.testekscluster,aws_eks_node_group.testeksclusternode]
+}
